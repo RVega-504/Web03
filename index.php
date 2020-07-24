73a3257d2d66b5eb693698a996e1f8479e03a719
elGitHuduwu <63244544+elGitHuduwu@users.noreply.github.com>
2020-07-23 22:27:51 -0400

    Version 1

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="Estilo/CSS.css" rel="stylesheet" type="text/css"/>
+        <script src="script.js" type="text/javascript"></script>
+        <meta charset="UTF-8">
+        <title>Validador</title>
+    </head>
+    <body>
+        <div>
+            <h2>Validador de rut</h2><br>
+            <label class="mensaje">RUT (Sin puntos)</label><br>
+            <input type="number" id="rut" placeholder="Ingrese RUT">
+            <label>-</label>
+            <select id="dv">
+                <option value="" disabled selected>DV</option>
+                <option value="11">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="k">k</option>
+            </select><br>
+            <button class="boton" onclick="validar();">Validar</button><br>
+            <label class="mensaje" id="test"></label>
+        </div>
+    </body>
+</html>
---
+++ script.js
@@ -0,0 +1,30 @@
+function validar(){
+    var rut = document.getElementById('rut').value;
+
+    var serieNum = 2;
+    var suma = 0;
+    
+    for (i=rut.length-1; i>=0; i--){ 
+        suma = suma + (rut.charAt(i) * serieNum);	
+        console.log(rut.charAt(i) * serieNum);
+	if (serieNum == 7){			
+            serieNum = 2;
+        } else {
+        serieNum ++;
+        }
+    }
+    
+    var res=11-(suma%11);
+    console.log("el dv es:" + res);
+    
+    var dv = document.getElementById('dv').value;
+    
+    if(dv == res){
+        document.getElementById('test').innerHTML = "El rut es válido";
+    } else if (dv=='k' && res==10){
+        document.getElementById('test').innerHTML = "El rut es válido";
+    } else{
+        alert("RUT no valido");
+        document.getElementById('test').innerHTML = "El rut no es válido";
+    }
+}
